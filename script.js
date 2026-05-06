c89c21b35af9b353087766d5bbc4aec89b186ee0
Muhammad-Azeem123 <mianazeem5605@gmail.com>
2026-05-06 06:43:50 +0000

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+// This is a JavaScript script that prints "Hello, World!" to the console using Node.js.
+
+function main() {
+    /**
+     * The main function of our Hello World application.
+     * It simply prints the greeting to the standard output.
+     */
+    console.log("Hello, World!");
+}
+
+// Call the main function to start the application.
+main();
